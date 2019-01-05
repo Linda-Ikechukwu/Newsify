f6a2e1a1d5885fe2c126f634724948544d53784c
Linda Ikechukwu <lindaedwin96@gmail.com>
2019-01-05 05:21:38 +0100

    added sticky functionalities to the header

--- app.js
+++ app.js
@@ -5,16 +5,20 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + '/public'));
 
+
 //ROUTES
 
-app.get("/", function(req,res){
+app.get("/", (req,res) =>{
   res.redirect("/news");
 });
 
+app.get("/404", (req,res) =>{
+  res.render("404");
+});
 
-app.get("/news", function(req, res){
-  let url = "https://newsapi.org/v2/top-headlines?language=en&source!=reddit&apiKey=9b2406747cdb42a4bccfa4a7967da150&pageSize=100";
-  request.get(url,function(error,response,body){
+app.get("/news", (req, res)=>{
+  let url = "https://newsapi.org/v2/top-headlines?language=en&source!=reddit&apiKey=9b2406747cdb42a4bccfa4a7967da150&pageSize=99";
+  request.get(url,(error,response,body)=>{
     if(!error && response.statusCode == 200){
       let data = JSON.parse(body);
       res.render("home", {data: data});
@@ -22,29 +26,28 @@ app.get("/news", function(req, res){
       res.render("404");
     }
   });
+  app.get("/news/:title", (req,res)=>{
+    let title = req.params.title;
+    request.get(url,(error, response, body)=>{
+      if(!error && response.statusCode == 200){
+        let data = JSON.parse(body);
+        res.render("show",{data:data, title:title});
+  
+  
+      }else{
+        res.render("404");
+      }
+    });  
    
+  }); 
 });
 
-app.get("/news/headlines/:title", function(req,res){
-  let title = req.params.title;
-  let url = "https://newsapi.org/v2/top-headlines?language=en&source!=reddit&apiKey=9b2406747cdb42a4bccfa4a7967da150&pageSize=100";
-  request.get(url,function(error, response, body){
-    if(!error && response.statusCode == 200){
-      let data = JSON.parse(body);
-      res.render("show",{data:data, title:title});
 
 
-    }else{
-      res.render("404");
-    }
-  });  
- 
-});
-
-app.get("/news/keyword",function(req,res){
+app.get("/news/keyword",(req,res)=>{
    let keywordResults = req.query.keyword;
    let url = "https://newsapi.org/v2/everything?language=en&q="+keywordResults+"&apiKey=9b2406747cdb42a4bccfa4a7967da150&pageSize=100"; 
-   request.get(url,function(error, response, body){
+   request.get(url,(error, response, body)=>{
      if(!error && response.statusCode == 200){
          let data = JSON.parse(body);
          res.render("keyword", {data: data});
@@ -52,11 +55,9 @@ app.get("/news/keyword",function(req,res){
        res.render("404");
      }
    });
-   app.get("/news/keyword/:title", function(req,res){
+   app.get("/news/keyword/:title", (req,res)=>{
     let title = req.params.title;
-    //let keywordResults = req.query.keyword;
-    let url = "https://newsapi.org/v2/everything?language=en&q="+keywordResults+"&apiKey=9b2406747cdb42a4bccfa4a7967da150&pageSize=100";
-    request.get(url,function(error, response, body){
+    request.get(url,(error, response, body)=>{
       if(!error && response.statusCode == 200){
         let data = JSON.parse(body);
         res.render("show",{data:data, title:title});
@@ -70,10 +71,10 @@ app.get("/news/keyword",function(req,res){
   });
 });
 
-app.get("/news/category", function(req,res){
+app.get("/news/category", (req,res)=>{
   let categoryResult = req.query.category;
   let url = "https://newsapi.org/v2/top-headlines?country=ng&category="+categoryResult+"&apiKey=9b2406747cdb42a4bccfa4a7967da150&pageSize=30";
-  request.get(url,function(error, response, body){
+  request.get(url,(error, response, body)=>{
     if(!error && response.statusCode == 200){
         let data = JSON.parse(body);
         res.render("category", {data: data});
@@ -81,12 +82,9 @@ app.get("/news/category", function(req,res){
       res.render("404");
     }
   });
-  app.get("/news/category/:title", function(req,res){
+  app.get("/news/category/:title", (req,res)=>{
     let title = req.params.title;
-    //let categoryResult = req.query.category;
-    let url = "https://newsapi.org/v2/top-headlines?country=ng&category="+categoryResult+"&apiKey=9b2406747cdb42a4bccfa4a7967da150&pageSize=100";
-    console.log(url);
-    request.get(url,function(error, response, body){
+    request.get(url,(error, response, body)=>{
       if(!error && response.statusCode == 200){
         let data = JSON.parse(body);
         res.render("show",{data:data, title:title});
--- public/Js/index.js
+++ public/Js/index.js
@@ -1,1 +1,36 @@
+const header = document.querySelector(".header");
+const topBtn = document.querySelector(".top");
+const filterForm1 = document.querySelector(".filter-form1");
+const filterForm2 = document.querySelector(".filter-form2");
+window.addEventListener('scroll', sticky = () => {
+  if( window.pageYOffset >= 270){
+    header.classList.add('header-sticky');
+    filterForm1.style.display = "none";
+    filterForm2.style.display = "none";
+  }else{
+    header.classList.remove('header-sticky');
+    filterForm1.style.display = "block";
+    filterForm2.style.display = "block";
+  }
+});
+
+topBtn.addEventListener("click", scrollTop = () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+} );
+
+
+function showTopBtn(){
+  if (document.body.scrollTop > 3000 || document.documentElement.scrollTop > 3000){
+    topBtn.style.display = "block";
+  }
+  else{
+    topBtn.style.display = "none";
+  }
+}
+
+
+
+
+
 
